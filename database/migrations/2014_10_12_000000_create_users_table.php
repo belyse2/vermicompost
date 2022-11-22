47a59f6a695bd3808f9a0f07c966b3c35c493f27
unknown <belyse2>
2022-11-22 08:38:03 -0800

    some user  correction

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,11 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string("Fname");
-            $table->string("Lname");
-            $table->string("location");
-            $table->string("Gender");
-            $table->string("Email")->unique();
+            $table->string("name");
+            $table->string("location")->nullable();
+            $table->string("gender")->nullable();
+            $table->string("email")->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,15 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\studentController;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\BinController;
+use App\Http\Controllers\VermUserController;
+use App\Http\Controllers\usercontroller;
+use App\Http\Controllers\BinConditionController;
+use App\Http\Controllers\loginController;
+use App\Http\Controllers\WormTypeController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,6 +35,37 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
-Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+//public routes
+
+
+//Route::post('/register',[AuthController::class,'register']);
+Route::get('/User/search/{Fname}',[usercontroller::class,'search']);
+Route::get('/Bin/search/{name}',[BinController::class,'search']);
+Route::get('/BinCondition/search/{}',[BinConditionController::class,'search']);
+Route::get('/WormType/search/{}',[WormTypeController::class,'search']);
+Route::get('/User/search/{Fname}',[usercontroller::class,'search']);
+
+
+
+
+
+// protected routes
+
+
+Route::group(['middleware' =>['auth:sanctum']], function () {
+
+
+
+//Route::post('/logout',[AuthController::class,'logout']);
+Route::resource("/User",App\Http\usercontroller::class);
+Route::resource("/Bin",App\Http\Controllers\BinController::class);
+Route::resource("/BinCondition",App\Http\BinConditionController::class);
+Route::resource("/WormType",App\Http\WormTypeController::class);
+Route::resource("/User",App\Http\usercontroller::class);
+
+
+});
