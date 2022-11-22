87e6b3f62c59138fb41cd17a4f2a5ba857e34697
KIMHON1 <honore.kimenyi@gmail.com>
2022-11-22 20:49:14 +0200

    displaying bin content

--- resources/views/pages/bins.blade.php
+++ resources/views/pages/bins.blade.php
@@ -3,8 +3,8 @@
 
 
    @section('content')
-        <h1>Number of Bins</h1>
-        <table>
+   
+        {{-- <table>
             <tr>
                 <td>number</td>
                 <td>location</td>
@@ -16,7 +16,41 @@
             
             </tr>
         @endforeach
-    </table>
+    </table> --}}
+
+    <table class="table">
+        <thead class="thead-dark">
+          <tr>
+           
+            <th scope="col">Bin_number</th>
+            <th scope="col">Bin_location</th>
+            <th scope="col">Bin_created_at</th>
+            <th scope="col">Updated_at</th>
+           
+          </tr>
+        </thead>
+        <tbody>
+
+        @foreach ($data as $data)
+          <tr>
+           
+           
+            <td>{{$data->number}}</td>
+            <td>{{$data->location}}</td>
+            <td>{{$data->created_at}}</td>
+            <td>{{$data->updated_at}}</td>
+
+          </tr>
+        @endforeach
+         
+       
+        </tbody>
+      </table>
+      
+  
+     
+      </table>
+      
     @endsection('content')
 
 
